cfca096e58357fa516258695587cd58a1984bcc7
gaganPrasadOnGitHub <writetogaganprasad@gmail.com>
2024-10-15 01:48:11 +0530

    Add - Analytics

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
@@ -14,6 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default app;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import LanguageSelector from "./ui/LanguageSelector";
 import { useTranslation } from "react-i18next";
+import ReactGA from "react-ga";
 
 export const Header: React.FC = () => {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    ReactGA.initialize("G-0R4PGR37EN");
+  }, []);
+
   return (
     <header className="flex items-center justify-between mt-4">
       <Link to="/" className="flex flex-col items-center ">
